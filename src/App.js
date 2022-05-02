e2ec5b673644a3f4f07afe6f922a1303c87cfe9e
Bindu <binduvukyam@gmail.com>
2022-05-02 16:41:41 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ;ughljkasghkkksdjhsjkghsjk;dghk;jas
         </a>
       </header>
     </div>
